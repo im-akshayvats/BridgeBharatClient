8e8c95f67f4057e54a468bad0d66261e609f3360
im-akshayvats <akshayvats2000@gmail.com>
2022-04-05 14:46:51 +0530

    Created Loading Page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Loader from './components/Loader/Loader.jsx';
 
+const Home = React.lazy(() => import('./components/Home/Home.jsx'));
+
 export default function App() {
-  return (
-    <Loader />
-  )
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
+
+  useEffect(() => {
+    let timer = 50;
+    const intervalId = setTimeout(() => {
+      setLoadingPercentage(loadingPercentage + 1)
+    }, timer);
+    if (loadingPercentage === 100) {
+      setIsLoaded(true)
+      clearInterval(intervalId);
+    }
+  }, [loadingPercentage])
+
+  if (isLoaded) {
+    return <Home />
+  }
+  return <Loader percentage={loadingPercentage} />
 }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box } from '@mui/material';
 
-export default function Loader() {
+import Logo from '../assets/Images/logo_white.svg';
+
+import './style.scss';
+
+export default function Loader({percentage}) {
   return (
-    <div>Loader</div>
+    <Box className='loadingScreenContent'>
+      <img
+        src={Logo}
+        alt="Logo"
+        className='loadingLogo'
+        style={{
+          height: 144
+        }}
+      />
+      <Typography
+        variant='body1'
+        className='loadingSubHeading'
+        mt={1}
+      >
+        Experience the ease of digital wholesale buying
+      </Typography>
+      <Typography
+        variant='h1'
+        className='loadingPercentage'
+        mt={3}
+      >
+        {percentage}%
+      </Typography>
+    </Box>
   )
 }
