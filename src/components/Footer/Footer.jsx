a29ca4447c097c99874c795f376ac73f8eb6e256
im-akshayvats <akshayvats2000@gmail.com>
2022-04-08 12:59:24 +0530

    Created Footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Typography, Box } from '@mui/material';
 
 import './style.css';
+import './style.scss';
+import paymentMethod from '../assets/images/paymentMethod.svg'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <Typography variant='h6' className='footerLink'>
+        Sitemap
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Summary
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        T&C
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Copyright
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Contact us
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Social Media
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Payment Information
+      </Typography>
+      <Typography variant='h6' className='footerLink'>
+        Cancellation and refund
+      </Typography>
+      <Box className='paymentMethodImage'>
+        <img src={paymentMethod} alt="Payment Methods" />
+      </Box>
+    </footer>
   )
 }
