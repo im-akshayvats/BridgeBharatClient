0e1eb36d9baf65ef2b1aff311350b3bd980be974
im-akshayvats <akshayvats2000@gmail.com>
2022-04-08 09:32:14 +0530

    Created quote section.

--- src/components/Home/Quote/Quote.jsx
+++ src/components/Home/Quote/Quote.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Box, Container, Typography } from '@mui/material';
 
 import './style.css';
+import './style.scss';
 
 export default function Quote() {
   return (
-    <div>Quote</div>
+    <section className="quote">
+      <Container maxWidth={'md'} className='quoteContainer' sx={{pb: 16}}>
+        <Box className='quoteImage'>
+
+        </Box>
+        <Box className='quoteContent'>
+          <Typography variant='h5' className='quoteText'>
+            Ttinxacidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrut accums]sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euis
+          </Typography>
+        </Box>
+      </Container>
+    </section>
   )
 }
