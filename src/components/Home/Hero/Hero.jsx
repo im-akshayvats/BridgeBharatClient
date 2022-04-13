463aaff8d4a8d5478bfd07af60be3d79c2edb077
im-akshayvats <akshayvats2000@gmail.com>
2022-04-13 12:04:59 +0530

    Created hero section.

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Typography, Box, Button } from '@mui/material';
 
 import './style.scss';
+import banner from '../assets/images/banner.svg';
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className="hero">
+      <img src={banner} alt="Banner" />
+      <Typography variant='h3' className='heading'>
+        YOUR <span className='desi'>DESI</span> WHOLESALE PARTNER
+      </Typography>
+      <Typography variant='h6' className='subHeading'>
+        INDIAN HANDCRAFTED PRODUCTS FROM 4000+ ARTISANAL COMMUNITIES
+      </Typography>
+      <Box className='buttonContainer'>
+        <Button variant="outlined">
+          <Typography>
+            Catalog
+          </Typography>
+        </Button>
+        <Button variant="outlined">
+          <Typography>
+            Customize
+          </Typography>
+        </Button>
+        <Button variant="outlined">
+          <Typography>
+            Connect
+          </Typography>
+        </Button>
+      </Box>
+    </section>
   )
 }
