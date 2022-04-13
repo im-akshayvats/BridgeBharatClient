e24f0be8f3e376f65af0a048556889853606f7c9
im-akshayvats <akshayvats2000@gmail.com>
2022-04-13 14:45:59 +0530

    Updated Style
    1. Changed the font-family and color.
    2. Update the Container location in Homepage.

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Box, Button } from '@mui/material';
+import { Typography, Box, Button, Container } from '@mui/material';
 
 import './style.scss';
 import banner from '../assets/images/banner.svg';
@@ -7,30 +7,32 @@ import banner from '../assets/images/banner.svg';
 export default function Hero() {
   return (
     <section className="hero">
-      <img src={banner} alt="Banner" />
-      <Typography variant='h3' className='heading'>
-        YOUR <span className='desi'>DESI</span> WHOLESALE PARTNER
-      </Typography>
-      <Typography variant='h6' className='subHeading'>
-        INDIAN HANDCRAFTED PRODUCTS FROM 4000+ ARTISANAL COMMUNITIES
-      </Typography>
-      <Box className='buttonContainer'>
-        <Button variant="outlined">
-          <Typography>
-            Catalog
-          </Typography>
-        </Button>
-        <Button variant="outlined">
-          <Typography>
-            Customize
-          </Typography>
-        </Button>
-        <Button variant="outlined">
-          <Typography>
-            Connect
-          </Typography>
-        </Button>
-      </Box>
+      <Container>
+        <img src={banner} alt="Banner" />
+        <Typography variant='h3' className='heading'>
+          YOUR <span className='desi'>DESI</span> WHOLESALE PARTNER
+        </Typography>
+        <Typography variant='h6' className='subHeading'>
+          INDIAN HANDCRAFTED PRODUCTS FROM 4000+ ARTISANAL COMMUNITIES
+        </Typography>
+        <Box className='buttonContainer'>
+          <Button variant="outlined">
+            <Typography>
+              Catalog
+            </Typography>
+          </Button>
+          <Button variant="outlined">
+            <Typography>
+              Customize
+            </Typography>
+          </Button>
+          <Button variant="outlined">
+            <Typography>
+              Connect
+            </Typography>
+          </Button>
+        </Box>
+      </Container>
     </section>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from '@mui/material';
 
 import Hero from './Hero/Hero';
 import Introduciton from './Introduction/Introduction';
@@ -7,10 +6,8 @@ import Introduciton from './Introduction/Introduction';
 export default function Home() {
   return (
     <main>
-      <Container>
-        <Hero />
-        <Introduciton />
-      </Container>
+      <Hero />
+      <Introduciton />
     </main>
   )
 }
