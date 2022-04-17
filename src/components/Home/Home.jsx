eaf61f7aa01885da4261817ad241f132f42a5c5e
im-akshayvats <akshayvats2000@gmail.com>
2022-04-17 14:15:40 +0530

    Created product-catalog section.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from './Hero/Hero';
 import Introduciton from './Introduction/Introduction';
+import ProductCatalog from './ProductCatalog/ProductCatalog';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Introduciton />
+      <ProductCatalog />
     </main>
   )
 }
