be5a1428249b5f973ef1c0fc8d85bbe949faf30f
im-akshayvats <akshayvats2000@gmail.com>
2022-04-17 17:02:02 +0530

    created a template for commercial messages and added 2 more messages

--- src/components/Home/CommercialMessages/CommercialMessages.jsx
+++ src/components/Home/CommercialMessages/CommercialMessages.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
-import EndToEnd from './EndToEnd/EndToEnd';
+import MessageTemplate from './MessageTemplate/MessageTemplate';
 
 import './style.scss';
+import endToEnd from '../assets/images/endToEndPic.gif';
+import customization from '../assets/videos/customizationVideo.m4v';
+import marketing from '../assets/images/marketing.svg';
 
 export default function CommercialMessages() {
   return (
     <section className="commercialMessages">
-      <EndToEnd />
+      <MessageTemplate
+        heading={'End-To-End Wholesale'}
+        description={'Leave us to do heavy lifting while you focus on you.'}
+        highlightedDescription={'Experience the ease of digital wholesale ecosystem'}
+        image={endToEnd}
+      />
+      <MessageTemplate
+        heading={'Easy to use customization tools'}
+        description={'Customize products that tell the story of your brand.'}
+        highlightedDescription={' You understand your market, we understand you.'}
+        video={customization}
+      />
+      <MessageTemplate
+        heading={'Exclusive Marketing Collateral'}
+        description={'From our grassroot research and relationships, get access to our detailed guide of history and process behind the making of the product.'}
+        highlightedDescription={'Offer your customer a complete experience of Indian products'}
+        image={marketing}
+      />
     </section>
   )
 }
---
+++ src/components/Home/CommercialMessages/MessageTemplate/MessageTemplate.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Container, Typography } from '@mui/material';
+
+import './style.scss'
+
+export default function MessageTemplate({heading, description, highlightedDescription, video, image}) {
+  return (
+    <section className="message">
+      <Container className='container'>
+        <Box className='content'>
+          <Typography variant='h1' className='heading'>
+            {heading}
+          </Typography>
+          <Typography variant='h5' className='description'>
+            {description} <span className='fontOrange'>{highlightedDescription}</span>
+          </Typography>
+        </Box>
+        <Box className='videoBox'>
+          {
+            video ? 
+            (<video autoplay="true" muted loop>
+              <source src={video} />
+            </video>) : 
+            <img src={image} alt="pic" />
+          }
+        </Box>
+      </Container>
+    </section>
+  )
+}
