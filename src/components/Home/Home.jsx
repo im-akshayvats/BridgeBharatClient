0deedcc59064b90d88a51b7bd3b2ec1b3337faed
im-akshayvats <akshayvats2000@gmail.com>
2022-04-18 21:30:56 +0530

    Added another product catalog to the homepage.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
       <Introduciton />
       <ProductCatalog />
       <CommercialMessages />
+      <ProductCatalog />
       <Solutions />
       <Partners />
       <Testimonials />
