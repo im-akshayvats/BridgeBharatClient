a6f2d038cdb45e5f47a8ac1b17711b33a933957f
im-akshayvats <akshayvats2000@gmail.com>
2022-04-25 16:26:46 +0530

    Made header responsive.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 
 import './style.scss';
 import logo from '../assets/images/logo.svg'
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    if (isOpen) {
+      return setIsOpen(false)
+    }
+    return setIsOpen(true)
+  };
+  const smallNavigation = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      smallNavigation.current.style.height = '150px';
+    } else {
+      smallNavigation.current.style.height = '0px';
+    }
+  })
+
   return (
-    <Container>
-      <header>
+    <header>
+      <Container className="navBarContainer">
+        <div className="hamburger" onClick={handleClick}>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+        </div>
         <nav>
           <Typography variant='h6' className='navLink'>ABOUT US</Typography>
           <Typography variant='h6' className='navLink'>CATALOG</Typography>
           <Typography variant='h6' className='navLink'>CONTACT US</Typography>
         </nav>
         <img src={logo} alt="BBLogo" />
-      </header>
-    </Container>
+      </Container>
+      <Container>
+        <nav className='smallNavigation' ref={smallNavigation}>
+          <Typography variant='h6' className='navLink'>ABOUT US</Typography>
+          <Typography variant='h6' className='navLink'>CATALOG</Typography>
+          <Typography variant='h6' className='navLink'>CONTACT US</Typography>
+        </nav>
+      </Container>
+    </header>
   )
 }
