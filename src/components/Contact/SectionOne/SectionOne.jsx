28cee7b3353b0d31d8a9214ddd2f5772837b3e44
im-akshayvats <akshayvats2000@gmail.com>
2022-05-02 12:23:41 +0530

    Created sectionOne.

--- src/components/Contact/SectionOne/SectionOne.jsx
+++ src/components/Contact/SectionOne/SectionOne.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+
+import { Container, Typography, Button } from '@mui/material';
+
 import './style.scss';
+import border from '../../../assets/border.png';
+import cowCart from '../../../assets/cowCart.png';
+import whatsAppIcon from '../../../assets/whatsAppIcon.png';
 
 export default function SectionOne() {
   return (
     <section className="sectionOne">
-      SectionOne
+      <img src={border} alt="cowCart" />
+      <Container className="container">
+        <Typography variant="h3" className="heading">
+          Looking to source Indian<br />Local Products?
+        </Typography>
+        <img src={cowCart} alt="" />
+        <Typography variant='h5' className="subHeading">
+          Start you journey now with us!
+        </Typography>
+        <Button variant='outlined' className="btn">
+          GET IN TOUCH
+        </Button>
+      </Container>
+      <img src={border} alt="cowCart" />
+      <div className="whatsAppContainer">
+        <Typography variant="h6" className="whatsAppContent">
+          Talk directly to<br />our representative
+        </Typography>
+        <img src={whatsAppIcon} alt="whatsAppIcon" />
+      </div>
     </section>
   )
 }
