70b45f8a02fbcc57d6aaaed37d66e9cd6fb0a934
im-akshayvats <akshayvats2000@gmail.com>
2022-05-10 11:16:39 +0530

    Created team page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Story from './Story/Story';
+import Team from './Team/Team';
 
 export default function About() {
   return (
     <main>
       <Story />
+      <Team />
     </main>
   )
 }
