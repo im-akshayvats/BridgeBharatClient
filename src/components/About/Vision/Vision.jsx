01460a7e8bb60b68d2d56e67dff395f38409c034
im-akshayvats <akshayvats2000@gmail.com>
2022-05-12 20:53:57 +0530

    Made vision responsive.

--- src/components/About/Vision/Vision.jsx
+++ src/components/About/Vision/Vision.jsx
@@ -19,6 +19,7 @@ export default function Vision() {
           <Typography variant="h6" className="description">
             Our VISION is for Bridge Bharat & its partners to become leaders in driving positive change in the artisanal economy and trade of Indian handicrafts by simplifying discovery, supply chains and process of customization with our digital tools and services.
           </Typography>
+          <a href="https://prairie-koi-94e.notion.site/Careers-at-Bridge-Bharat-7cf90f2a3c9a4c8f809ea91129decc55" target="_blank" rel="noopener noreferrer">Read More</a>
         </div>
       </Container>
     </section>
