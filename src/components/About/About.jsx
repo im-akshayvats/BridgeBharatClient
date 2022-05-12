ee6f2ddb0097f281278c2acf1e4e998cd80fadde
im-akshayvats <akshayvats2000@gmail.com>
2022-05-12 20:35:00 +0530

    create section one about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SectionOne from './SectionOne/SectionOne';
 import Vision from './Vision/Vision';
 import Mission from './Mission/Mission';
 import Team from './Team/Team';
@@ -7,6 +8,7 @@ import Team from './Team/Team';
 export default function About() {
   return (
     <main>
+      <SectionOne />
       <Vision />
       <Mission />
       <Team />
---
+++ src/components/About/SectionOne/SectionOne.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+import './style.scss';
+
+import camel from '../../../assets/camel.png';
+const blueLogo = 'https://d2dm1r7c3wtyk.cloudfront.net/images/home/blue.svg';
+const orangeLogo = 'https://d2dm1r7c3wtyk.cloudfront.net/images/home/orange.svg'
+const yellowLogo = 'https://d2dm1r7c3wtyk.cloudfront.net/images/home/yellow.svg';
+
+export default function SectionOne() {
+  return (
+    <section className="sectionAboutOne section">
+      <Container className="container">
+        <img src={camel} alt="camel" />
+        <Typography variant="h6" className="heading">
+          YOUR SOURCING & WHOLESALE PARTNER FOR INDIAN<br />HERITAGE PRODUCTS AND ART
+        </Typography>
+        <div className="motiveContainer">
+          <div className="motive">
+            <img src={blueLogo} alt="blueLogo" />
+            <Typography variant='body2' className="name">DIGITAL</Typography>
+          </div>
+          <div className="motive">
+            <img src={orangeLogo} alt="orangeLogo" />
+            <Typography variant='body2' className="name">RELIABLE</Typography>
+          </div>
+          <div className="motive">
+            <img src={yellowLogo} alt="yellowLogo" />
+            <Typography variant='body2' className="name">RESPONSIBLE</Typography>
+          </div>
+          <div className="motive">
+            <img src={orangeLogo} alt="orangeLogo" />
+            <Typography variant='body2' className="name">PERSONAL</Typography>
+          </div>
+          <div className="motive">
+            <img src={blueLogo} alt="blueLogo" />
+            <Typography variant='body2' className="name">HANDPICKED</Typography>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
