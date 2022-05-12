befb4d0ba2dc67057270fd2d27a90d73a5afe205
im-akshayvats <akshayvats2000@gmail.com>
2022-05-12 18:30:16 +0530

    Created team section.

--- src/components/About/Team/Team.jsx
+++ src/components/About/Team/Team.jsx
@@ -4,6 +4,14 @@ import { Container, Typography } from '@mui/material';
 import './style.scss';
 import Member from './Member/Member';
 
+import mansi from '../../../assets/mansi.png';
+import anupriyaDubey from '../../../assets/anupriyaDubey.png';
+import durga from '../../../assets/durga.png';
+import parul from '../../../assets/parul.png';
+import anisha from '../../../assets/anisha.png';
+import anupriya from '../../../assets/anupriya.png';
+import akshay from '../../../assets/akshay.png';
+
 const aakanksha = 'https://d2dm1r7c3wtyk.cloudfront.net/images/about/aakanksha.png'
 
 export default function Team() {
@@ -15,47 +23,59 @@ export default function Team() {
             Team
           </Typography>
         </div>
-        <div className="membersContainer">
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
-          <Member
-            image={aakanksha}
-            name="AAKANKSHA SINGH"
-            designation={'Founder'}
-          />
+        <div className="members">
+          <div className="membersFirstContainer">
+            <Member
+              image={aakanksha}
+              name="AAKANKSHA SINGH"
+              designation={'Founder and CEO'}
+              className="member"
+            />
+            <Member
+              image={mansi}
+              name="MANSI SHEKHAWAT"
+              designation={'Operations Lead'}
+              className="member"
+            />
+            <Member
+              image={anupriyaDubey}
+              name="ANUPRIYA DUBEY"
+              designation={'Artist Engagement'}
+              className="member"
+            />
+            <Member
+              image={durga}
+              name="DURGA VENKATESAN "
+              designation={'Social Media Lead'}
+              className="member"
+            />
+          </div>
+          <div className="membersSecondContainer">
+            <Member
+              image={parul}
+              name="PARUL SOOD"
+              designation={'Marketing and Communications'}
+              className="member"
+            />
+            <Member
+              image={anisha}
+              name="ANISHA BHUJBAL"
+              designation={'Visual Design Lead'}
+              className="member"
+            />
+            <Member
+              image={anupriya}
+              name="ANUPRIYA"
+              designation={'Front-End Developer'}
+              className="member"
+            />
+            <Member
+              image={akshay}
+              name="AKSHAY VATS"
+              designation={'Full Stack Developer'}
+              className="member"
+            />
+          </div>
         </div>
       </Container>
     </section>
